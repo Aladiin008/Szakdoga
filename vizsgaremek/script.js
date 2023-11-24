c61f54e665c5cf28bdaefdf44ba39fd89f013a0a
dmajaa <151950981+dmajaa@users.noreply.github.com>
2023-11-24 21:56:44 +0100

    Add files via upload

---
+++ vizsgaremek/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navLinks = document.getElementById("links");
+
+    window.menuMegjelenites = function() {
+        navLinks.style.right = "-50%";
+        ujMenu(true);
+    };
+
+    window.menuBezaras = function() {
+        navLinks.style.right = "-100%";
+        ujMenu(false);
+    };
+
+    function ujMenu(megjelenitve) {
+        var orokbefogadhatoLink = document.querySelector("#links ul li:nth-child(2) a");
+        if (orokbefogadhatoLink) {
+            orokbefogadhatoLink.innerHTML = megjelenitve ? "Örökbefogadható<br> állataink" : "Örökbefogadható állataink";
+        }
+    }
+
+   
+});
+
+
